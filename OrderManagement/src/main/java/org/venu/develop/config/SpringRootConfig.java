a73f9e9d522bbfe94da59c118e70015d013f1f87
Venu <dvenu@carolina.rr.com>
2015-09-25 13:26:08 -0400

    base package changed

--- OrderManagement/src/main/java/org/venu/develop/config/SpringRootConfig.java
+++ OrderManagement/src/main/java/org/venu/develop/config/SpringRootConfig.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({ "org.venu.develop.service" })
+@ComponentScan({ "org.venu.develop" })
 public class SpringRootConfig {
 }
